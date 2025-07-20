d685f16e2517f1c4dca8cd29b6b812f0e9cc5822
Chitwan Singh <119756817+chitwan27@users.noreply.github.com>
2025-07-20 14:12:54 +0530

    ios option

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -10,3 +10,21 @@ export default function rootLayout() {
   )
 }
 
+
+// import "../global.css";
+// import { Stack } from "expo-router";
+// import { View } from "react-native";
+
+// export default function RootLayout() {
+//   return (
+//     <View className="flex-1 bg-zinc-200 dark:bg-zinc-800 m-safe-offset-0">
+//       <Stack
+//         screenOptions={{
+//           headerShown: true, // show default iOS back button
+//           headerStyle: { backgroundColor: "#1e293b" }, // optional styling
+//           headerTintColor: "white", // back button + title color
+//         }}
+//       />
+//     </View>
+//   );
+// }
